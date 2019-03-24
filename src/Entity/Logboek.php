9b7d0dc9f8b8a0000fa01ffeecd02eecc70c89d8
Giovannivr <giovannivr@live.com>
2019-03-24 19:18:48 +0100

    Add files via upload

---
+++ src/Entity/Logboek.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LogboekRepository")
+ */
+class Logboek
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="logboekusers")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $userid;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $brief_nr;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datum;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="logboekchauffeur")
+     */
+    private $chauffeurId;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Truck", inversedBy="logboektruck")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $truckId;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $kubs;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $laadplaats;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $vertrektijd;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $bestemming;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $evenement;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserid(): ?User
+    {
+        return $this->userid;
+    }
+
+    public function setUserid(?User $userid): self
+    {
+        $this->userid = $userid;
+
+        return $this;
+    }
+
+    public function getBriefNr(): ?int
+    {
+        return $this->brief_nr;
+    }
+
+    public function setBriefNr(int $brief_nr): self
+    {
+        $this->brief_nr = $brief_nr;
+
+        return $this;
+    }
+
+    public function getDatum(): ?\DateTimeInterface
+    {
+        return $this->datum;
+    }
+
+    public function setDatum(\DateTimeInterface $datum): self
+    {
+        $this->datum = $datum;
+
+        return $this;
+    }
+
+    public function getChauffeurId(): ?User
+    {
+        return $this->chauffeurId;
+    }
+
+    public function setChauffeurId(?User $chauffeurId): self
+    {
+        $this->chauffeurId = $chauffeurId;
+
+        return $this;
+    }
+
+    public function getTruckId(): ?Truck
+    {
+        return $this->truckId;
+    }
+
+    public function setTruckId(?Truck $truckId): self
+    {
+        $this->truckId = $truckId;
+
+        return $this;
+    }
+
+    public function getKubs(): ?int
+    {
+        return $this->kubs;
+    }
+
+    public function setKubs(int $kubs): self
+    {
+        $this->kubs = $kubs;
+
+        return $this;
+    }
+
+    public function getLaadplaats(): ?string
+    {
+        return $this->laadplaats;
+    }
+
+    public function setLaadplaats(string $laadplaats): self
+    {
+        $this->laadplaats = $laadplaats;
+
+        return $this;
+    }
+
+    public function getVertrektijd(): ?\DateTimeInterface
+    {
+        return $this->vertrektijd;
+    }
+
+    public function setVertrektijd(\DateTimeInterface $vertrektijd): self
+    {
+        $this->vertrektijd = $vertrektijd;
+
+        return $this;
+    }
+
+    public function getBestemming(): ?string
+    {
+        return $this->bestemming;
+    }
+
+    public function setBestemming(string $bestemming): self
+    {
+        $this->bestemming = $bestemming;
+
+        return $this;
+    }
+
+    public function getEvenement(): ?string
+    {
+        return $this->evenement;
+    }
+
+    public function setEvenement(?string $evenement): self
+    {
+        $this->evenement = $evenement;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,102 @@
+<?php
+// src/Entity/User.php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Logboek", mappedBy="userid")
+     */
+    private $logboekusers;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Logboek", mappedBy="chauffeurId")
+     */
+    private $logboekchauffeur;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->logboekusers = new ArrayCollection();
+        $this->logboekchauffeur = new ArrayCollection();
+        // your own logic
+    }
+
+    /**
+     * @return Collection|Logboek[]
+     */
+    public function getLogboekusers(): Collection
+    {
+        return $this->logboekusers;
+    }
+
+    public function addLogboekuser(Logboek $logboekuser): self
+    {
+        if (!$this->logboekusers->contains($logboekuser)) {
+            $this->logboekusers[] = $logboekuser;
+            $logboekuser->setUserid($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLogboekuser(Logboek $logboekuser): self
+    {
+        if ($this->logboekusers->contains($logboekuser)) {
+            $this->logboekusers->removeElement($logboekuser);
+            // set the owning side to null (unless already changed)
+            if ($logboekuser->getUserid() === $this) {
+                $logboekuser->setUserid(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Logboek[]
+     */
+    public function getLogboekchauffeur(): Collection
+    {
+        return $this->logboekchauffeur;
+    }
+
+    public function addLogboekchauffeur(Logboek $logboekchauffeur): self
+    {
+        if (!$this->logboekchauffeur->contains($logboekchauffeur)) {
+            $this->logboekchauffeur[] = $logboekchauffeur;
+            $logboekchauffeur->setChauffeurId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLogboekchauffeur(Logboek $logboekchauffeur): self
+    {
+        if ($this->logboekchauffeur->contains($logboekchauffeur)) {
+            $this->logboekchauffeur->removeElement($logboekchauffeur);
+            // set the owning side to null (unless already changed)
+            if ($logboekchauffeur->getChauffeurId() === $this) {
+                $logboekchauffeur->setChauffeurId(null);
+            }
+        }
+
+        return $this;
+    }
+}
